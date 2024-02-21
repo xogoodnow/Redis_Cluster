dcdfc18b764cfbb518229dfbe98472d6082096ed
xogoodnow <xo4ever9999@gmail.com>
2024-02-21 12:55:34 +0330

    Added role path

--- Terraform/modules/Redis/main.tf
+++ Terraform/modules/Redis/main.tf
@@ -1,7 +1,7 @@
 
 resource "null_resource" "Redis" {
   provisioner "local-exec" {
-    command = "sleep 50  && PWD='../' ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ../Ansible/inventory ../Ansible/playbooks/Deploy.yaml --private-key sshkey/private_key.pem"
+    command = "sleep 50  && PWD='../' ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH='../Ansible/roles/' ansible-playbook -i ../Ansible/inventory ../Ansible/playbooks/Deploy.yaml --private-key sshkey/private_key.pem"
   }
 }
 
